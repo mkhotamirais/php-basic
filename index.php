e014ebc72a243def9acf58b43faf982b464bbd77
Mkhotamirais <mkhotamirais@gmail.com>
2024-10-23 20:05:16 +0700

    mulai belajar php lagi

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        nav {
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>PHP basic</h1>
+    <p>Untuk memulai belajar php kamu harus memiliki lingkungan pengembangan PHP, cara paling mudah dan umum digunakan adalah dengan download dan install XAMPP</p>
+    <p>Buka XAMPP Control Panel lalu jalankan 'apache'</p>
+    <nav>
+        <a href="less1.php">less1</a>
+        <a href="less1.php">less2</a>
+    </nav>
+</body>
+
+</html>
---
+++ less1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>less1</h1>
+    <ol>
+        <li>
+            Opening, closing tag and echo <br>
+            &lt;?php echo 'hello, world!'; ?&gt; <br>
+            <?php echo "hello world"; ?>
+        </li>
+        <li>Seemua syntax yang ada di antara opening dan closing tag PHP adalah code php, jadi php bisa bercampur dengan HTML di bagian manapun bisa disisipkan sekalipun hanya satu variabel.</li>
+        <li>
+            Variable: <br>
+            &lt;?php $name = "ahmad"; ?&gt; <br>
+            <?php $name = "ahmad"; ?>
+            hello <?php echo $name; ?>
+        </li>
+        <li>
+            DataTypes: (from now, view the source code to learn the PHP syntax)
+
+        </li>
+    </ol>
+</body>
+
+</html>
