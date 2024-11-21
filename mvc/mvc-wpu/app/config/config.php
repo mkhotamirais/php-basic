672eb3caad2f7bc600c116c730cf789a42c2a590
mkhotamirais <mkhotamirais@gmail.com>
2024-11-22 05:07:12 +0700

    update config.php

--- mvc/mvc-wpu/app/config/config.php
+++ mvc/mvc-wpu/app/config/config.php
@@ -1,12 +1,12 @@
 <?php
 
-const BASE_URL = "http://localhost/php-basic/mvc/mvc-wpu/public";
-
+// const BASE_URL = "http://localhost/php-basic/mvc/mvc-wpu/public";
 // const DB_HOST = "localhost";
 // const DB_USER = "root";
 // const DB_NAME = "db_wpu_pdo";
 // const DB_PASS = "";
 
+const BASE_URL = "https://tamionweb.my.id/php-basic/mvc/mvc-wpu/public";
 const DB_HOST = "localhost";
 const DB_USER = "tami8172_admin";
 const DB_PASS = "tami8172_admin";
