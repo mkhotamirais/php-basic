ccde8b0c5d08ac90a1bc50715b2b02b7f85ab836
mkhotamirais <mkhotamirais@gmail.com>
2024-11-22 05:15:36 +0700

    err satu angka

--- mvc/mvc-wpu/app/config/config.php
+++ mvc/mvc-wpu/app/config/config.php
@@ -10,4 +10,4 @@ const BASE_URL = "https://tamionweb.my.id/php-basic/mvc/mvc-wpu/public";
 const DB_HOST = "localhost";
 const DB_USER = "tami8172_admin";
 const DB_PASS = "tami8172_admin";
-const DB_NAME = "tami7172_db_wpu_pdo";
+const DB_NAME = "tami8172_db_wpu_pdo";
