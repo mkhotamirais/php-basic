8fda1ea3766efef3c5102c7275f46b3a5f5cd443
mkhotamirais <mkhotamirais@gmail.com>
2024-11-23 15:25:34 +0700

    lupa ganti config

--- db/muba-mysqli/config/config.php
+++ db/muba-mysqli/config/config.php
@@ -2,16 +2,16 @@
 
 // development
 $base_url = "http://localhost/php-basic/db/muba-mysqli";
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "db_muba_mysqli";
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "db_muba_mysqli";
 
 // production
-// $hostname = "localhost";
-// $username = "tami8172_admin";
-// $password = "tami8172_admin";
-// $database = "tami8172_db_muba_mysqli";
+$hostname = "localhost";
+$username = "tami8172_admin";
+$password = "tami8172_admin";
+$database = "tami8172_db_muba_mysqli";
 
 
 $db = mysqli_connect($hostname, $username, $password, $database);
